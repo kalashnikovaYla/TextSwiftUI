992374caf13f144b3de0f8ce5c51709355d875c5
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-09 16:52:27 +0500

    Initial

--- TextSwiftUI/ContentView.swift
+++ TextSwiftUI/ContentView.swift
@@ -10,10 +10,27 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+                
+            HStack(alignment: .top) {
+                Text("Hello")
+                    .baselineOffset(-10)
+                    .border(Color.red)
+                Text("Hello")
+                    .border(Color.green)
+                Text("Hello")
+                    .baselineOffset(10)
+                    .border(Color.blue)
+            }
+            .background(Color(white: 0.9))
+                //.multilineTextAlignment(.trailing)
+                //.lineLimit(nil)
+                //.background(.red)
+                //.foregroundColor(.white)
+                //.lineSpacing(16)
+                //.font(.system(size: 29, weight: .bold, design: .monospaced))
+                //.frame(width: 400, height: 200, alignment: .trailing)
+                //.minimumScaleFactor(0.1)
         }
         .padding()
     }
