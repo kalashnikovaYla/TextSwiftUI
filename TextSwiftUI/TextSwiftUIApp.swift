cd344d4f4eeceaa3dd0df00ac16898bd0480f938
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-09 16:10:53 +0500

    Initial Commit

---
+++ TextSwiftUI/TextSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextSwiftUIApp.swift
+//  TextSwiftUI
+//
+//  Created by sss on 09.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TextSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
